ee646ff623f1f34769e97709c7006563bf4592a6
Mongnhi17422 <99971773+Mongnhi17422@users.noreply.github.com>
2022-10-28 21:14:19 +0700

    BT

---
+++ app.js
@@ -0,0 +1,107 @@
+// Câu 1: D
+// Câu 2: C
+// Câu 3: B
+// Câu 4: A
+// Câu 5: A
+// Câu 6: A
+// Câu 7: D
+// Câu 8: B
+
+
+
+// // Câu 9:
+// let m = [1,2,3,4,5,6, "hh", "9",80,100];
+
+// // Cách 1:
+// m.splice(6, 2)
+// console.log(m);
+
+// // Cách 2:
+// // console.log(
+// //     m.filter(item => !["hh", "9"].includes(item))
+// // )
+// let mSquared = m.map(item => pow(item,2));
+// console.log(mSquared)
+
+// // Câu 10:
+// // let string =  "High knowledge, high return";
+// // console.log(string);
+// // let newString = string.split(' ');
+// // console.log(newString);
+
+
+
+// Câu 11:
+class People {
+    constructor(_name, _age, _address) {
+        this.name = _name;
+    this.age = _age;
+    this.address = _address
+    }
+    set(
+
+    )
+    get(
+
+    )
+}
+
+class Students extends People {
+    constructor(_name, _age, _address, _id, _math,_physical, _chemistry) {
+        super(_name, _age, _address);
+        this.id = _id;
+        this.math = _math;
+        this.physical = _physical;
+        this.chemistry = _chemistry;
+    }
+
+    set() {
+        super.set();
+    }
+    
+    get(){
+        super.get();
+    }
+
+    GPA()
+
+
+}
+
+// // Câu 12:
+// class hinh {
+//     constructor(_dai, _rong){
+//         this.dai = dai;
+//         this.rong = rong;
+//     }
+// }
+
+// // Câu 13:
+class animal {
+    constructor(_name, _age)
+}
+
+
+// // Câu 14:
+
+class kA{
+    constructor(_toan,_ly,_hoa) {
+        this.toan = _toan;
+        this.ly = _ly;
+        this.hoa =_hoa
+    }
+}
+
+
+class kB{
+    constructor(toan_,_hoa,_sinh) {
+        this.toan = _toan;
+        this.hoa =_hoa;
+        this.sinh = _sinh;
+    }
+}
+
+
+
+
+
