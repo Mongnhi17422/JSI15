ba14296ceb64a1b48c9e8f93ee56160416d0136c
Mongnhi17422 <99971773+Mongnhi17422@users.noreply.github.com>
2022-11-18 21:06:18 +0700

    BT_checkpoint2

--- app.js
+++ app.js
@@ -1,107 +1,19 @@
-// Câu 1: D
-// Câu 2: C
-// Câu 3: B
-// Câu 4: A
-// Câu 5: A
-// Câu 6: A
-// Câu 7: D
-// Câu 8: B
-
-
-
-// // Câu 9:
-// let m = [1,2,3,4,5,6, "hh", "9",80,100];
-
-// // Cách 1:
-// m.splice(6, 2)
-// console.log(m);
-
-// // Cách 2:
-// // console.log(
-// //     m.filter(item => !["hh", "9"].includes(item))
-// // )
-// let mSquared = m.map(item => pow(item,2));
-// console.log(mSquared)
-
-// // Câu 10:
-// // let string =  "High knowledge, high return";
-// // console.log(string);
-// // let newString = string.split(' ');
-// // console.log(newString);
-
-
-
-// Câu 11:
-class People {
-    constructor(_name, _age, _address) {
-        this.name = _name;
-    this.age = _age;
-    this.address = _address
+let clear;
+let getValue = function getValue(){
+    let minute = document.getElementById('minute').value;
+    let second = document.getElementById('second').value;
+    second++;
+    if (second==60){
+        minute = minute + 1 && second == 0 
     }
-    set(
-
-    )
-    get(
-
-    )
-}
-
-class Students extends People {
-    constructor(_name, _age, _address, _id, _math,_physical, _chemistry) {
-        super(_name, _age, _address);
-        this.id = _id;
-        this.math = _math;
-        this.physical = _physical;
-        this.chemistry = _chemistry;
-    }
-
-    set() {
-        super.set();
-    }
-    
-    get(){
-        super.get();
-    }
-
-    GPA()
-
+    document.getElementById('minute').value = minute;
+    document.getElementById('second').value = second;
 
 }
+function start(){
+    clear = setInterval(getValue,1);
 
-// // Câu 12:
-// class hinh {
-//     constructor(_dai, _rong){
-//         this.dai = dai;
-//         this.rong = rong;
-//     }
-// }
-
-// // Câu 13:
-class animal {
-    constructor(_name, _age)
 }
-
-
-// // Câu 14:
-
-class kA{
-    constructor(_toan,_ly,_hoa) {
-        this.toan = _toan;
-        this.ly = _ly;
-        this.hoa =_hoa
-    }
+function stop (){
+    clearInterval(clear);
 }
-
-
-class kB{
-    constructor(toan_,_hoa,_sinh) {
-        this.toan = _toan;
-        this.hoa =_hoa;
-        this.sinh = _sinh;
-    }
-}
-
-
-
-
-
