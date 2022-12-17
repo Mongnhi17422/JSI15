06f0bf5d507738bf0cd31841c092426b8480e21a
Mongnhi17422 <99971773+Mongnhi17422@users.noreply.github.com>
2022-12-17 23:32:04 +0700

    New code Js

---
+++ assets/JAVASCRIPT/validator.js
@@ -0,0 +1,28 @@
+function Validator(options) {
+    let formElement = document.querySelector(options.form);
+    console.log(options.rules);
+    if (formElement) {
+        options.rules.forEach (function (rule) {
+
+        });
+    }
+}
+
+Validator.isRequired = function (selector) {
+    return {
+        selector: selector,
+        test: function () {
+
+        }
+    };
+}
+
+Validator.isEmail = function (selector) {
+    return {
+        selector: selector,
+        test: function () {
+
+        }
+    };
+
+}
