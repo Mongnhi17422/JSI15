a3c99bf7f71be1bf6bbaf6ff6b55e7ff79d3e215
Mongnhi17422 <99971773+Mongnhi17422@users.noreply.github.com>
2022-12-23 10:49:47 +0700

    doneCode Week13

--- assets/JAVASCRIPT/app.js
+++ assets/JAVASCRIPT/app.js
@@ -1,71 +1,71 @@
-// // Shop
+// // // Shop
 
-let cartIcon = document.querySelector('#cart-icon');
-let cart = document.querySelector('.cart');
-let Closecart = document.querySelector('#close-cart')
+// let cartIcon = document.querySelector('#cart-icon');
+// let cart = document.querySelector('.cart');
+// let Closecart = document.querySelector('#close-cart')
 
-cartIcon.onclick = () => {
-   cart.classList.add("active");
-};
+// cartIcon.onclick = () => {
+//    cart.classList.add("active");
+// };
 
-Closecart.onclick = () => {
-   cart.classList.romove("active");
-};
-
-
-
-if (document.readyState == 'loading') {
-   document.addEventListener('DOMContentLoaded', ready)
-}
-else {
-   ready ();
-}
-
-
-function ready (){
-   var removeCartButtons = document.getElementsByClassName('cart-remove')
-   console.log(removeCartButtons);
-   for (let i = 0; i < removeCartButtons.length; i++) {
-      let button = removeCartButtons[i]
-      button.addEventListener('click', removeCartItem);
-   }
-}
-
-function removeCartItem(event) {
-   var buttonClicked = event.target;
-   buttonClicked.parentElement.remove();
-}
+// Closecart.onclick = () => {
+//    cart.classList.romove("active");
+// };
 
 
 
+// if (document.readyState == 'loading') {
+//    document.addEventListener('DOMContentLoaded', ready)
+// }
+// else {
+//    ready ();
+// }
 
 
-// SignUp - Login
+// function ready (){
+//    var removeCartButtons = document.getElementsByClassName('cart-remove')
+//    console.log(removeCartButtons);
+//    for (let i = 0; i < removeCartButtons.length; i++) {
+//       let button = removeCartButtons[i]
+//       button.addEventListener('click', removeCartItem);
+//    }
+// }
 
-if (!localStorage.userList) {
-   localStorage.userList = JSON.stringify([]);
-}
+// function removeCartItem(event) {
+//    var buttonClicked = event.target;
+//    buttonClicked.parentElement.remove();
+// }
 
-let fullname = document.getElementById("fullname");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
-let signUp = document.getElementById("signUp");
 
-signUp.addEventListener("click", () => {
-   let userList = JSON.parse(localStorage.userList);
-   for (let i = 0; i<userList.length; i++) {
-      if (userList.fullname == fullname.value || userList.email == email.value) {
-         alert("Tài khoản đã tồn tại");
-         return;
-      }
-   }
-   userList.push({
-      fullname: fullname.value,
-      email: email.value,
-      password: password.value,
-   });
-   localStorage.userList = JSON.stringify(userList);
-   alert("Đăng ký thành công!")
-});
+
+
+
+// // SignUp - Login
+
+// if (!localStorage.userList) {
+//    localStorage.userList = JSON.stringify([]);
+// }
+
+// let fullname = document.getElementById("fullname");
+// let email = document.getElementById("email");
+// let password = document.getElementById("password");
+// let signUp = document.getElementById("signUp");
+
+// signUp.addEventListener("click", () => {
+//    let userList = JSON.parse(localStorage.userList);
+//    for (let i = 0; i<userList.length; i++) {
+//       if (userList.fullname == fullname.value || userList.email == email.value) {
+//          alert("Tài khoản đã tồn tại");
+//          return;
+//       }
+//    }
+//    userList.push({
+//       fullname: fullname.value,
+//       email: email.value,
+//       password: password.value,
+//    });
+//    localStorage.userList = JSON.stringify(userList);
+//    alert("Đăng ký thành công!")
+// });
 
 
--- assets/JAVASCRIPT/validator.js
+++ assets/JAVASCRIPT/validator.js
@@ -1,9 +1,31 @@
 function Validator(options) {
+
+    function validate(inputElement, rule) {
+        var errorElement = inputElement.parentElement.querySelector(options.errorSelector);
+
+        var errorMessage = rule.test(inputElement.value);
+
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid');
+        }
+    }
+
     let formElement = document.querySelector(options.form);
     console.log(options.rules);
     if (formElement) {
         options.rules.forEach (function (rule) {
+            
+            var inputElement = formElement.querySelector(rule.selector);
 
+            if (inputElement) {
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+                }
+            }
         });
     }
 }
@@ -11,8 +33,8 @@ function Validator(options) {
 Validator.isRequired = function (selector) {
     return {
         selector: selector,
-        test: function () {
-
+        test: function (value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này!'
         }
     };
 }
@@ -20,9 +42,30 @@ Validator.isRequired = function (selector) {
 Validator.isEmail = function (selector) {
     return {
         selector: selector,
-        test: function () {
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined : 'Vui lòng nhập email!';
 
         }
     };
 
+}
+
+
+Validator.minLength = function (selector, min) {
+    return {
+        selector: selector,
+        test: function (value)  {
+            return value.length >= min ? undefined : `Mật khẩu tối thiếu ${min} kí tự!`;
+        }
+    }
+}
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue() ? undefined : message || 'Vui lòng nhập lại mật khẩu!'
+        }
+    }
 }
